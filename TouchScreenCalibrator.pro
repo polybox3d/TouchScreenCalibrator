21d4d38133a29e8053c2599f5db6e69e87228219
poly <poly@localhost.localdomain>
2015-03-16 17:41:41 +0100

    Add FontAwsome

--- TouchScreenCalibrator.pro
+++ TouchScreenCalibrator.pro
@@ -13,7 +13,7 @@ TEMPLATE = app
 
 #-------------- WE ADD GIT VERSION----------------
 
-GIT_VERSION = $$system(git --git-dir $$PWD/../.git --work-tree $$PWD describe --always --tags)
+GIT_VERSION = $$system(git --git-dir $$PWD/.git --work-tree $$PWD describe --always --tags)
 DEFINES += GIT_VERSION=\\\"$$GIT_VERSION\\\"
 
 SOURCES += main.cpp\
--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <QApplication>
 #include <QCoreApplication>
 #include <QSettings>
+#include <QMessageBox>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
@@ -15,9 +17,32 @@ int main(int argc, char *argv[])
     /***  GIT_VERSION defined in .pro file ***/
     QCoreApplication::setApplicationVersion( GIT_VERSION );
 
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
 
-    return a.exec();
+    QApplication a(argc, argv);
+    /***
+     *  Check If XINPUT and XINPUT_CALIBRATOR are isntalled :)
+     *
+     * **/
+    /** Font Awsome Version 4.3.0  -- http://fortawesome.github.io/Font-Awesome/ -- **/
+    QFontDatabase::addApplicationFont(":/fontawesome-webfont.ttf");
+
+    if ( ! QFile::exists("/usr/bin/xinput") )
+    {
+        QMessageBox::critical(0, QWidget::tr("Error, missing package: xinput"), QWidget::tr("Error, xinput package is not installed.  \n \n Use this command :  \t sudo apt-get install xinput \n") );
+    }
+    else if ( ! QFile::exists( QDir::homePath()+ "/xinput_calibrator") )
+    {
+        QMessageBox::critical(0, QWidget::tr("Error, missing folder: xinput_calibrator"), QWidget::tr("Error, xinput_calibrator folder not found.  \n \n Commands :  \t cd ~; git clone https://github.com/tias/xinput_calibrator.git \n") );
+    }
+    else if ( ! QFile::exists( QDir::homePath()+ "/xinput_calibrator/src/xinput_calibrator") )
+    {
+        QMessageBox::critical(0, QWidget::tr("Error, missing executable: xinput_calibrator"), QWidget::tr("Error, xinput_calibrator executable not found.  \n \n Use this command :  \n\n cd ~/xinput_calibrator && ./autogen.sh && make  \n") );
+    }
+    else
+    {
+        MainWindow w;
+        w.show();
+
+        return a.exec();
+    }
 }
